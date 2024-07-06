32dc9fe6ad99c9465e53b4e45d2977d481b5e064
caiosaboia <caiorubems@gmail.com>
2024-07-06 14:03:01 -0300

    Termino do arquivo de listas e incio de tuplas

---
+++ ED/trabalhando_lista.py
@@ -0,0 +1,19 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+# frutas = []
+# print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+# carro = ["ferrari", "f8", 4200000, 2020, 2900, "são paulo", true]
+# print(carro)
+
+
+print("Uso do pop: \n")
+print(frutas.pop())
+print(frutas)
