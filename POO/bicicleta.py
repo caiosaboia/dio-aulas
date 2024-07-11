22788a3030427332bdcbe4a7238c1d669aa55579
caiosaboia <caiorubems@gmail.com>
2024-07-10 22:00:00 -0300

    atualizando a bicicleta

--- POO/bicicleta.py
+++ POO/bicicleta.py
@@ -4,6 +4,7 @@ class Bicicleta:
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        self.marcha = 1
 
     def buzinar(self):
         print("(Buzinando...)")
@@ -20,6 +21,16 @@ class Bicicleta:
     #     Bicicleta: {self.cor}, {self.modelo}, {self.ano}, {self.valor}
     #     """
 
+    def trocar_marcha(self, n_marcha):
+
+        print("Trocando marcha...")
+
+        def _trocar_macha():
+            if n_marcha > self.marcha:
+                print("Marcha trocada")
+            else:
+                print("Marcha nao foi trocada")
+
     def __str__(self):
         return f"{
         self.__class__.__name__}:{
@@ -28,3 +39,7 @@ class Bicicleta:
 
 
 b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+b1.buzinar()
+b1.trocar_marcha(2)
+b1.correr()
+b1.parar()
